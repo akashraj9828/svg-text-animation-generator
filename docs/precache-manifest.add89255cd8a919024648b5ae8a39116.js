681f116bdd12cc7bdf0fad1ca8ecee2b1bdf2501
Akash Raj <akashraj9828@gmail.com>
2024-03-03 15:58:44 +0000

    update build

---
+++ docs/precache-manifest.add89255cd8a919024648b5ae8a39116.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86ec265ee5ec50a2d32859a67db546f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f5dcc924b4f024d9983",
+    "url": "./static/css/2.aa61572a.chunk.css"
+  },
+  {
+    "revision": "11850166c7dff10c327d",
+    "url": "./static/css/main.5f031b11.chunk.css"
+  },
+  {
+    "revision": "2f5dcc924b4f024d9983",
+    "url": "./static/js/2.f1f644e0.chunk.js"
+  },
+  {
+    "revision": "d6d981ba3418d25f42ce6f2188fd7513",
+    "url": "./static/js/2.f1f644e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11850166c7dff10c327d",
+    "url": "./static/js/main.d7910924.chunk.js"
+  },
+  {
+    "revision": "e918d53b3a170e12301e",
+    "url": "./static/js/runtime-main.2b67384d.js"
+  },
+  {
+    "revision": "925fc5db7f7fe4cda81aa8d2cd932238",
+    "url": "./static/media/upload.925fc5db.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.47ee44bdf1eb3e5593ca3671d859b005.js"
+  "./precache-manifest.add89255cd8a919024648b5ae8a39116.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
